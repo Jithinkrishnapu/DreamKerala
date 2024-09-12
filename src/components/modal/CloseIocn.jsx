6c1a9b01d8ce69cb28be817a1432bff924862b08
jithinkrishna <jithin.mykare@gmail.com>
2024-09-12 19:49:33 +0530

    clean ups

---
+++ src/components/modal/CloseIocn.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+export default function CloseIocn() {
+  return (
+<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="Group 48096910">
+<path id="Vector 13" d="M1.25238 0.996094L12.5811 12.3252" stroke="#484848" stroke-width="1.5" stroke-linecap="round"/>
+<path id="Vector 14" d="M12.5815 0.996094L1.25238 12.3248" stroke="#484848" stroke-width="1.5" stroke-linecap="round"/>
+</g>
+</svg>
+  )
+}
--- src/components/modal/ModalComponent.jsx
+++ src/components/modal/ModalComponent.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import CloseIcon from "./CloseIocn.jsx";
+import CloseIocn from "./CloseIocn.jsx";
 
 function ModalComponent({ children, showModal, setShowModal }) {
   const handleClose = () => setShowModal(false);
@@ -14,9 +16,20 @@ function ModalComponent({ children, showModal, setShowModal }) {
         show={showModal}
         onHide={handleClose}
       >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Book Your Vacation Trip
+        <Modal.Header>
+          <Modal.Title
+            className="d-flex w-100 px-3 flex-row justify-content-between"
+            id="contained-modal-title-vcenter"
+          >
+            <span>Book Your Trip</span>
+            <button
+              onClick={handleClose}
+              type="button"
+              class="close bg-transparent"
+              aria-label="Close"
+            >
+             <CloseIocn size={12} />
+            </button>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>{children}</Modal.Body>
