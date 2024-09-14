bb1cb99da7c3a0bd645258acfd5be06340bada86
jithinkrishna <jithin.mykare@gmail.com>
2024-09-14 21:42:22 +0530

    api added

--- pages/api/destination.js
+++ pages/api/destination.js
@@ -1,30 +1,30 @@
-// import { NextResponse } from 'next/server';
-// import { Client } from '@notionhq/client';
+import { NextResponse } from 'next/server';
+import { Client } from '@notionhq/client';
 
-// const notion = new Client({ auth: process.env.NOTION_KEY });
-// const NOTION_DB = process.env.NOTION_DB;
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const NOTION_DB = process.env.NOTION_DB;
 
-// // Main handler function
-// export default async function handler(req, res) {
-//     if (req.method === 'GET') {
-//         return handleGet(req, res);
-//     } else {
-//         // Handle other methods or return a 405 Method Not Allowed
-//         res.setHeader('Allow', ['GET']);
-//         return res.status(405).end(`Method ${req.method} Not Allowed`);
-//     }
-// }
+// Main handler function
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        return handleGet(req, res);
+    } else {
+        // Handle other methods or return a 405 Method Not Allowed
+        res.setHeader('Allow', ['GET']);
+        return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
 
-// // Function to handle GET requests
-// async function handleGet(req, res) {
-//     try {
-//         const response = await notion.databases.query({
-//             database_id: NOTION_DB
-//         });
+// Function to handle GET requests
+async function handleGet(req, res) {
+    try {
+        const response = await notion.databases.query({
+            database_id: NOTION_DB
+        });
 
-//         return res.status(200).json(response.results);
-//     } catch (error) {
-//         console.error("Error fetching Notion data: ", error);
-//         return res.status(500).json({ error: "Error fetching Notion data" });
-//     }
-// }
+        return res.status(200).json(response.results);
+    } catch (error) {
+        console.error("Error fetching Notion data: ", error);
+        return res.status(500).json({ error: "Error fetching Notion data" });
+    }
+}
