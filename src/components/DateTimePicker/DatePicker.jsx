b51a49e270193522fe032337b1064257370f47b5
Shirazjaf <ishirazjaf@gmail.com>
2024-09-16 20:55:19 +0530

    fix: date time styles

--- src/components/DateTimePicker/DatePicker.jsx
+++ src/components/DateTimePicker/DatePicker.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function DatePicker() {
   return (
-    <div class="date-picker mb-3 mb-md-0" tabindex="0">
-    <input name='Date' id='date' type="date" required />
+    <div style={{padding: '19px 25px', height:'63px'}} class="date-picker mb-3 mb-md-0" tabindex="0">
+    <input style={{padding:'20px'}} name='Date' id='date' type="date" required />
     <div class="placeholder"></div>
   </div>
   )
--- src/components/DateTimePicker/TimePicker.jsx
+++ src/components/DateTimePicker/TimePicker.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function TimePicker() {
   return (
-    <div class="time-picker" tabindex="0">
-    <input name='Time' id='time' type="time" required />
+    <div style={{padding: '19px 25px', height:'63px'}} class="time-picker" tabindex="0">
+    <input style={{padding:'20px'}} name='Time' id='time' type="time" required />
     <div class="placeholder"></div>
   </div>
   )
