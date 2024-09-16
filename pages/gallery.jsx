9ff9858fe3d3e4c2ec9f9f9af5d2dc9573bafce5
jithinkrishna <jithin.mykare@gmail.com>
2024-09-16 16:44:33 +0530

    gallery added

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -1,15 +1,39 @@
+import ModalComponent from "@/src/components/modal/ModalComponent";
 import PageBanner from "@/src/components/PageBanner";
+import Popupform from "@/src/components/popupform/Popupform";
 import Layout from "@/src/layout/Layout";
 import { galleryItems } from "@/src/utils/constants";
+import { useEffect, useState, useTransition } from "react";
 const Gallery = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    startTransition(async () => {
+      try {
+        const response = await fetch("/api/gallery");
+        if (!response.ok) {
+          throw new Error("Error fetching carousel data");
+        }
+        const result = await response.json();
+        setData(result[0]);
+      } catch (err) {
+        setError(err.message);
+      }
+    });
+  }, [startTransition]);
+
   return (
-    <Layout>
+    <Layout header={1} setShowModal={(val) => setShowModal(val)}>
       {/*====== Start Gallery Section ======*/}
       <section className="gallery-area pt-150 pb-70">
         <div className="container">
           <div className="row">
-            {galleryItems.map((item, index) => (
-              <GalleryItem key={index} {...item} />
+            {data?.properties?.images?.files?.map((item, index) => (
+              <GalleryItem key={index} imgSrc={item?.file.url} {...item} />
             ))}
           </div>
 
@@ -19,18 +43,22 @@ const Gallery = () => {
         </div>
       </section>
       {/*====== End Gallery Section ======*/}
+
+      <ModalComponent showModal={showModal} setShowModal={setShowModal}>
+        <Popupform />
+      </ModalComponent>
     </Layout>
   );
 };
 export default Gallery;
 
 const GalleryItem = ({ imgSrc, altText, linkHref, colClasses }) => (
-  <div className={colClasses}>
+  <div className="col-lg-4 col-md-6 col-sm-12">
     <div className="single-gallery-item mb-30 wow fadeInUp">
       <div className="gallery-img">
-        <img src={imgSrc} alt={altText} />
+        <img loading="lazy" src={imgSrc} alt={altText} />
         <div className="hover-overlay">
-          <a href={linkHref} className="icon-btn img-popup">
+          <a href={imgSrc} className="icon-btn img-popup">
             <i className="far fa-plus" />
           </a>
         </div>
