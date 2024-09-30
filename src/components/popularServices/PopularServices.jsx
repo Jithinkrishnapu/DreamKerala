9ed0554084079a82a484137babcb696cfbc512cd
jithinkrishna <jithin.mykare@gmail.com>
2024-09-30 09:37:45 +0530

    Fix(destination) fixed the description overflow

--- src/components/popularServices/PopularServices.jsx
+++ src/components/popularServices/PopularServices.jsx
@@ -63,7 +63,7 @@ function PopularServices() {
                   <p>
                     {
                       val?.properties?.Short_Description?.rich_text[0]
-                        ?.plain_text
+                        ?.plain_text?.substring(0, 80)+"..."
                     }
                   </p>
                   <img
