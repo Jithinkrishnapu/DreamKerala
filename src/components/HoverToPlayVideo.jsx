9af9a45a8b739fdfe3d4ddeea05df52efc94f731
jithinkrishna <jithin.mykare@gmail.com>
2024-11-06 09:47:43 +0530

    feat(HoverOnVideo) added a video in home

---
+++ src/components/HoverToPlayVideo.jsx
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react';
+import { Container } from 'react-bootstrap';
+
+const HoverPlayVideo = () => {
+  const videoRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+    }
+  };
+
+  return (
+    <Container className="w-100 my-5 rounded">
+      <video
+        ref={videoRef}
+        className="w-100 hover-to-play rounded"
+        autoPlay
+        loop
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <source src="/dream.mp4" type="video/mp4" />
+      </video>
+    </Container>
+  );
+};
+
+export default HoverPlayVideo;
